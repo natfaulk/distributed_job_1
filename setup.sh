3bc7c185829a650340cbc2fa12c09c71b1a36e2d
Ubuntu <n@test1.lo5xguilro1uffnnluxky4wn0d.xx.internal.cloudapp.net>
2021-01-28 23:46:07 +0000

    Add pip to setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-sudo apt-get install python3.8
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt update
+sudo apt -y install python3.8
+sudo apt -y install python3-pip
 
-python3.8 - m pip install -r requirements.txt
+python3.8 -m pip install pip
+python3.8 -m pip install -r requirements.txt
 
